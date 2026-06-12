d38425c9044110535758fb42d544d0b0c3e32b49
B G Kavinga <kavingas@adobe.com>
2026-06-12 16:28:44 +1000

    Refactor main.py to load environment variables for QiskitRuntimeService credentials

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# from qiskit_ibm_runtime import QiskitRuntimeService
+import os
 
-# QiskitRuntimeService.save_account(
-#     token="8um0XakhcOkB-3_dkhl63wfnrhfoo0QFsU9kcwD4x3lJ", # Use the 44-character API_KEY you created and saved from the IBM Quantum Platform Home dashboard
-#     instance="crn:v1:bluemix:public:quantum-computing:us-east:a/6c1a893ec8e24f6bae22c640fd310bbe:7a3fe693-588d-4220-95cf-7a542a483364::", # Optional
-# )
-
-
+from dotenv import load_dotenv
 from qiskit_ibm_runtime import QiskitRuntimeService
+
+load_dotenv()
+token    = os.getenv("TOKEN")
+instance = os.getenv("INSTANCE")
+QiskitRuntimeService.save_account(
+    token=token, # Use the 44-character API_KEY you created and saved from the IBM Quantum Platform Home dashboard
+    instance=instance, # Optional
+)
+
 
 # Run every time you need the service
 service = QiskitRuntimeService()
